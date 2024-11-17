2d4bca413f41b8800c12a6259d8284abb7ccc2f9
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-11-17 14:48:41 +0600

    update

---
+++ A_Say_Hello_With_C.c
@@ -0,0 +1,15 @@
+/**
+ *    author:  SayanDevNath
+ *    created: 14.11.2024 22:56:14
+ **/
+
+#include <stdio.h>
+int main()
+{
+
+    char a[100];
+    scanf("%s", &a);
+    printf("Hello, %s\n", a);
+
+    return 0;
+}
