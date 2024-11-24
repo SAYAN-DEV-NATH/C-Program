5dde01fd8b5e5b68a0f1420376c83ea812f8ad9a
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-11-24 17:19:46 +0600

    update

---
+++ Sheet #3/L_Max_Subarray.c
@@ -0,0 +1,25 @@
+/**
+ *    author:  SayanDevNath
+ *    created: 23.11.2024 23:06:55
+ **/
+#include <stdio.h>
+int main()
+{
+    int testcase;
+    scanf("%d", &testcase);
+    while (testcase--)
+    {
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+            printf("%d ", a[i]);
+        }
+
+        int i = 0, j = 0, mx;
+        for
+    }
+    return 0;
+}
